996d1c843069d4ae3f2012bed392b85e094cc35d
sheilsarda <sheilsarda@yahoo.com>
2020-08-23 21:52:17 +0000

    changed hello world to multi-threaded case

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <thread>
  /*
   * Compilation instructions
-  * $ g++ -o hello hello.cc -> $ ./hello
+  * $ g++ hello_world.cpp -std=c++0x -lpthread -o hello
+  * $ ./hello
   */
+
+/*
+ * every thread has initial function 
+ * where new thread execution begins
+ * for the main application-- this is main()
+ */
+void hello()
+{
+	std::cout<<"Hello concurrency world\n";
+}
 
 int main()
 {
-	// runs in a single thread
-	std::cout<< "Hello World\n";
+	// initializing thread object t
+	std::thread t(hello);
+	std::cout<< "Completed excution of main()\n";
+	// causes main() thread to wait for thread t
+	t.join();
 }
